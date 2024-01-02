d0c482d11ec8c2cb9ed28b9e2ea9a4bd6322db54
Jorge Vera <jorgevera988@gmail.com>
2024-01-02 18:11:15 -0500

    "back"

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 //background-image: url('https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2023/09/jujutsu-kaisen-expansion-de-dominio.jpg');
+//background-image: url('https://e1.pxfuel.com/desktop-wallpaper/828/291/desktop-wallpaper-bliss-windows-xp-windows-xp-night.jpg');
